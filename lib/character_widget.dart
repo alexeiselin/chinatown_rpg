96063a04cc2d053743785a06de28458464b4d00f
Simonov-Ilya <iSimonov48@yandex.ru>
2023-01-25 14:13:48 +0300

    character_widget changed

--- lib/character_widget.dart
+++ lib/character_widget.dart
@@ -1,31 +1,67 @@
 import 'package:flutter/material.dart';
 
-
-List<AssetImage> optionsList =  [
-const AssetImage('images/character_icons/image 200.png'),
-  const AssetImage('images/character_icons/image 404.png'),
-  const AssetImage('images/character_icons/image 2054.png'),
-  const AssetImage('images/character_icons/image 2095.png'),
-
+List<String> images = [
+  'images/character_icons/image 404.png',
+  'images/character_icons/image 2054.png',
+  'images/character_icons/image 2095.png',
+  'images/character_icons/image 200.png'
 ];
 
+List<String> texts = [
+  'Google employee.\nLoves dumplings and work burn outs.',
+  'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.',
+  'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.',
+  'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.'
+];
 
-class CharacterCard extends StatefulWidget {
-  const CharacterCard({Key? key}) : super(key: key);
+int activePage = 0;
 
-  @override
-  State<CharacterCard> createState() => _CharacterCardState();
+double heightForImage = 240;
+
+Column slider(images, pagePosition, active) {
+  double margin = active ? 0 : 50;
+  return Column(
+    children: [
+      AnimatedContainer(
+        height: active ? 140 : 70,
+        duration: const Duration(milliseconds: 500),
+        curve: Curves.easeInOutCubic,
+        margin: EdgeInsets.only(top: margin),
+        decoration: BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage(images[pagePosition]), fit: BoxFit.fitHeight),
+        ),
+      ),
+      const SizedBox(
+        height: 14,
+      ),
+      Align(
+        alignment: Alignment.bottomCenter,
+        child: active
+            ? Text(
+                texts[pagePosition],
+                textAlign: TextAlign.center,
+              )
+            : const SizedBox(),
+      )
+    ],
+  );
 }
 
-class _CharacterCardState extends State<CharacterCard> {
-  int _value = 0;
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-
-      ],
-    );
-  }
-}
+// class CharacterCard extends StatefulWidget {
+//   const CharacterCard({Key? key}) : super(key: key);
+//
+//   @override
+//   State<CharacterCard> createState() => _CharacterCardState();
+// }
+//
+// class _CharacterCardState extends State<CharacterCard> {
+//   int _value = 0;
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Column(
+//       children: [],
+//     );
+//   }
+// }
--- lib/pick_character.dart
+++ lib/pick_character.dart
@@ -1,3 +1,4 @@
+import 'package:chinatown_rpg/character_widget.dart';
 import 'package:flutter/material.dart';
 
 class PickCharacter extends StatefulWidget {
@@ -8,55 +9,6 @@ class PickCharacter extends StatefulWidget {
 }
 
 class _PickCharacterState extends State<PickCharacter> {
-  List<String> images = [
-    'images/character_icons/image 404.png',
-    'images/character_icons/image 2054.png',
-    'images/character_icons/image 2095.png',
-    'images/character_icons/image 200.png'
-  ];
-
-  List<String> texts = [
-    'Google employee.\nLoves dumplings and work burn outs.',
-    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.',
-    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.',
-    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.'
-  ];
-
-  int activePage = 0;
-
-  double heightForImage = 240;
-
-  Column slider(images, pagePosition, active) {
-    double margin = active ? 0 : 50;
-    return Column(
-      children: [
-        AnimatedContainer(
-          height: active ? 140 : 70,
-          // width: active ? 140 : 70,
-          duration: const Duration(milliseconds: 500),
-          curve: Curves.easeInOutCubic,
-          margin: EdgeInsets.only(top: margin),
-          decoration: BoxDecoration(
-            image: DecorationImage(
-                image: AssetImage(images[pagePosition]), fit: BoxFit.fitHeight),
-          ),
-        ),
-        const SizedBox(
-          height: 14,
-        ),
-        Align(
-          alignment: Alignment.bottomCenter,
-          child: active
-              ? Text(
-                  texts[pagePosition],
-                  textAlign: TextAlign.center,
-                )
-              : const SizedBox(),
-        )
-      ],
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -72,7 +24,8 @@ class _PickCharacterState extends State<PickCharacter> {
                 ? (555 / 222) * heightForImage
                 : double.infinity,
             child: PageView.builder(
-              // physics: ScrollPhysics(),
+              // padEnds: false, // for start from the beginning of the screen
+              physics: const BouncingScrollPhysics(),
               itemCount: images.length,
               pageSnapping: true,
               controller: PageController(viewportFraction: 0.32),
